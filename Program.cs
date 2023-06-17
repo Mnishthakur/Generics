1209d0719c4b3a297d4aeb4c3f09d7bb39868ce4
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 11:19:56 +0530

    [Manish Thakur]: Added  Refactor 1

--- Program.cs
+++ Program.cs
@@ -3,30 +3,31 @@ namespace Generics;
     {
         static void Main()
         {
-            TestMaximum(9, 2, 1);
-            TestMaximum(25.5f, 53.2f, 68.9f, -3.4f);
-            TestMaximum("apple", "banana", "cherry", "date");
+            Console.WriteLine("Test case 1: Max number is at first position");
+            int maxInt1 = FindMax<int>(9, 2, 1);
+            Console.WriteLine("Max number is: " + maxInt1); 
+
+            Console.WriteLine("Test case 2: Max number is at second position");
+            float maxFloat2 = FindMax<float>(25.5f, 53.2f, 68.9f);
+            Console.WriteLine("Max number is: " + maxFloat2); 
+
+            Console.WriteLine("Test case 3: Max string is at third position");
+            string maxString3 = FindMax<string>("apple", "banana", "cherry");
+            Console.WriteLine("Max string is: " + maxString3); 
         }
 
-        static void TestMaximum<T>(T a, T b, params T[] args) where T : IComparable
+        static T FindMax<T>(T a, T b, T c) where T : IComparable
         {
-            T max = FindMax(a, b, args);
-            PrintMax(max);
-        }
-
-        static T FindMax<T>(T a, T b, params T[] args) where T : IComparable
-        {
-            T[] arr = new T[args.Length + 2];
-            arr[0] = a;
-            arr[1] = b;
-            Array.Copy(args, 0, arr, 2, args.Length);
-            Array.Sort(arr);
-            return arr[^1];
-        }
-
-        static void PrintMax<T>(T max)
-        {
-            Console.WriteLine("Max value is: " + max);
+            T max = a;
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
         }
     }
 }
