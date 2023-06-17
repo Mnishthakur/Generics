e1897cc12023b76f83541bf96e8dbea061905f19
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 11:15:03 +0530

    [Manish Thakur]: Added  UC-4

--- Program.cs
+++ Program.cs
@@ -1,38 +1,28 @@
 namespace Generics;
-    class Program
     {
         static void Main()
         {
-            Console.WriteLine("Test case 1: Max string is at first position");
-            string max1 = FindMax("Apple", "Peach", "Banana");
-            Console.WriteLine("Max string is: " + max1); // 
+            Console.WriteLine("Test case 1: Max number is at first position");
+            int maxInt1 = FindMax(9, 2, 1);
+            Console.WriteLine("Max number is: " + maxInt1); // Output: Max number is: 9
 
-            Console.WriteLine("Test case 2: Max string is at second position");
-            string max2 = FindMax("Dog", "Elephant", "Cat");
-            Console.WriteLine("Max string is: " + max2); //
+            Console.WriteLine("Test case 2: Max number is at second position");
+            float maxFloat2 = FindMax(25.5f, 53.2f, 68.9f, -3.4f);
+            Console.WriteLine("Max number is: " + maxFloat2); // Output: Max number is: 68.9
 
             Console.WriteLine("Test case 3: Max string is at third position");
-            string max3 = FindMax("Green", "Red", "Yellow");
-            Console.WriteLine("Max string is: " + max3); //
+            string maxString3 = FindMax("apple", "banana", "cherry", "date");
+            Console.WriteLine("Max string is: " + maxString3); // Output: Max string is: date
         }
 
-        static string FindMax(string a, string b, string c)
+        static T FindMax<T>(T a, T b, params T[] args) where T : IComparable
         {
-            string max = null;
-            if (a.CompareTo(max) > 0)
-            {
-                max = a;
-            }
-            if (b.CompareTo(max) > 0)
-            {
-                max = b;
-            }
-            if (c.CompareTo(max) > 0)
-            {
-                max = c;
-            }
-            return max;
+            T[] arr = new T[args.Length + 2];
+            arr[0] = a;
+            arr[1] = b;
+            Array.Copy(args, 0, arr, 2, args.Length);
+            Array.Sort(arr);
+            return arr[^1];
         }
     }
 }
-
