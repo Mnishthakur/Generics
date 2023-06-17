e4c07fbb8b0983e06ff1545fe5116d1670cb7057
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 11:13:12 +0530

    [Manish Thakur]: Added  UC-3

--- Program.cs
+++ Program.cs
@@ -3,22 +3,22 @@ namespace Generics;
     {
         static void Main()
         {
-            Console.WriteLine("Test case 1: Max number is at first position");
-            float max1 = FindMax(10.5f, 2.3f, 1.1f);
-            Console.WriteLine("Max number is: " + max1); // Output: Max number is: 10.5
+            Console.WriteLine("Test case 1: Max string is at first position");
+            string max1 = FindMax("Apple", "Peach", "Banana");
+            Console.WriteLine("Max string is: " + max1); // 
 
-            Console.WriteLine("Test case 2: Max number is at second position");
-            float max2 = FindMax(5.7f, 53.2f, 2.8f);
-            Console.WriteLine("Max number is: " + max2); // Output: Max number is: 53.2
+            Console.WriteLine("Test case 2: Max string is at second position");
+            string max2 = FindMax("Dog", "Elephant", "Cat");
+            Console.WriteLine("Max string is: " + max2); //
 
-            Console.WriteLine("Test case 3: Max number is at third position");
-            float max3 = FindMax(10.1f, 3.2f, 14.8f);
-            Console.WriteLine("Max number is: " + max3); // Output: Max number is: 14.8
+            Console.WriteLine("Test case 3: Max string is at third position");
+            string max3 = FindMax("Green", "Red", "Yellow");
+            Console.WriteLine("Max string is: " + max3); //
         }
 
-        static float FindMax(float a, float b, float c)
+        static string FindMax(string a, string b, string c)
         {
-            float max = float.MinValue;
+            string max = null;
             if (a.CompareTo(max) > 0)
             {
                 max = a;
@@ -35,3 +35,4 @@ namespace Generics;
         }
     }
 }
+
